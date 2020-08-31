83fd131510abad18fd8cf09a8ac2eeb8d81cfd02
fstomazini <felipestomazini@gmail.com>
2020-08-31 11:45:40 -0300

    Inserção de usuario com post

--- src/main/java/com/felipespringmongo/demo/services/UserService.java
+++ src/main/java/com/felipespringmongo/demo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.felipespringmongo.demo.services;
 
 import com.felipespringmongo.demo.domain.User;
+import com.felipespringmongo.demo.dto.UserDTO;
 import com.felipespringmongo.demo.repository.UserRepository;
 import com.felipespringmongo.demo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,14 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+         return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 
 }
